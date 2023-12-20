5194625213c609f50f5ee2566aab779556d3bd01
SRIKHANTH R <139539680+Srikhanth14@users.noreply.github.com>
2023-12-21 00:02:54 +0530

    Add files via upload

---
+++ Dataset.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 20 23:31:24 2023
+
+@author: ELCOT
+"""
+
+import streamlit as st
+import pandas as pd
+def app():
+    st.title("Titanic Dataset Overview")
+
+    st.write(
+    """
+    Welcome to the Titanic Dataset Page. This dataset provides valuable information about
+    passengers aboard the Titanic, allowing us to explore and understand patterns related to
+    survival. It has undergone cleaning and exploratory data analysis (EDA) as part of the
+    project.
+    """)
+    
+    st.subheader("Data Source")
+    st.write("- Source: [Kaggle](https://www.kaggle.com/)")
+    st.write("- Dataset Credits: [Titanic Dataset](https://www.kaggle.com/datasets/brendan45774/test-file)")
+
+    st.subheader("Data Cleaning")
+    st.write("The dataset has been cleaned to ensure accurate and reliable analysis.")
+
+    st.subheader("Exploratory Data Analysis (EDA)")
+    st.write("""Exploratory data analysis has been conducted to gain insights into the dataset.
+    Visualizations and summaries are available in the project.""")
+
+    st.subheader("Sample Data")
+    st.write("Here is a snippet of the dataset, showcasing a few rows:")
+    data=pd.read_csv(r"D:\PROJECTS\Project_Files\tested.csv")
+    st.dataframe(data)
+
+    # Display your sample data here
+    st.subheader("Download Dataset")
+    st.write("You can download the full Titanic dataset for further exploration:")
+
+    def data_read(data):
+        return data.to_csv().encode('utf-8')
+    
+    csv=data_read(data)
+    
+    st.download_button(
+     label='Download Sample Data',
+        data=csv,
+        file_name='titanic.csv',
+        mime='text/csv'
+    )
+    
+    #st.markdown("[Download Titanic Dataset](csv_download_link)")
+    st.subheader("GitHub Repository")
+    st.write("For detailed information,and project code, you can explore the [GitHub repository](https://github.com/Srikhanth14/CODSOFT).")
+    st.write("Feel free to explore the dataset and check out the analysis conducted during the project!")
+    
+
---
+++ home.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 20 23:27:59 2023
+
+@author: ELCOT
+"""
+
+import streamlit as st
+from PIL import Image
+
+def app():
+   
+    image = Image.open(r'C:/Users/ELCOT/.spyder-py3/Model_Deployment/Boat.jpeg')
+    # Display the image in your Streamlit app
+    st.image(image,use_column_width=True)
+
+    st.markdown("# Titanic Survival Prediction")
+    # Welcome to Titanic Survival Prediction
+    st.write(
+    """
+    Welcome to Titanic Survival Prediction
+    Explore the fate of passengers aboard the Titanic! Our web app predicts whether a
+    passenger survived or not based on their details. Dive into the world of data science and
+    discover the insights from the tragic voyage.
+    """
+    )
+
+    st.subheader("Key Features")
+    st.write(
+    """
+    - Predict whether a passenger survived based on their details.
+    - Explore the dataset and uncover insights from the Titanic voyage.
+    - Easy-to-use interface to input passenger details and get predictions.
+    - Learn about the historic event of the Titanic and its predictive analysis.
+    """
+    )
+    st.subheader("Ready to Explore?")
+    st.write(
+    """
+    Get started now! Navigate through the tabs to learn more about the dataset, input
+    passenger details, and witness the predictive power of our model.
+    """
+    )
