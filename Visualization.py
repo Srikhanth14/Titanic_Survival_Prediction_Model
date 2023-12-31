045fd8d30f9ec991ce58b74cd3980f47ec77b071
SRIKHANTH R <139539680+Srikhanth14@users.noreply.github.com>
2023-12-31 14:33:59 +0530

    Update Visualization.py

--- Visualization.py
+++ Visualization.py
@@ -15,27 +15,27 @@ def visualization():
     
     # Image 1: Countplot Survival Distribution
     st.header("1. Countplot Survival Distribution")
-    image1 = Image.open("C:\\Users\\ELCOT\\.spyder-py3\\Model_Deployment\\titanic\\Survival_Distribution.png") 
+    image1 = Image.open("Survival_Distribution.png") 
     st.image(image1, caption="Survival Distribution", use_column_width=True)
     
     # Image 2: Age Distribution Histogram
     st.header("2. Age Distribution Histogram")
-    image2 = Image.open("C:\\Users\\ELCOT\\.spyder-py3\\Model_Deployment\\titanic\\Age_Distribution.png") 
+    image2 = Image.open("Age_Distribution.png") 
     st.image(image2, caption="Age Distribution", use_column_width=True)
     
     # Image 3: Fare Distribution Histogram
     st.header("3. Gender Distribution Barplot")
-    image3 = Image.open("C:\\Users\\ELCOT\\.spyder-py3\\Model_Deployment\\titanic\\Gender_Distribution.png") 
+    image3 = Image.open("Gender_Distribution.png") 
     st.image(image3, caption="Gender Distribution", use_column_width=True)
     
     # Image 4: Countplot Embarked Distribution
     st.header("4. Countplot Embarked Distribution")
-    image4 = Image.open("C:\\Users\\ELCOT\\.spyder-py3\\Model_Deployment\\titanic\\Embarked_Distribution.png") 
+    image4 = Image.open("Embarked_Distribution.png") 
     st.image(image4, caption="Embarked Distribution", use_column_width=True)
     
     # Image 5: Family Size Survival Rate Bar Plot
     st.header("5. Family Size Survival Rate Bar Plot")
-    image5 = Image.open("C:\\Users\\ELCOT\\.spyder-py3\\Model_Deployment\\titanic\\Family_Size.png") 
+    image5 = Image.open("Family_Size.png") 
     st.image(image5, caption="Family Size Survival Rate", use_column_width=True)
     
     # Call to Action
