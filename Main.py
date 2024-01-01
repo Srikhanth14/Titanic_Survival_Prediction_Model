4abb1882f9dcffef7909ecf61ff35bb7a48500bd
SRIKHANTH R <139539680+Srikhanth14@users.noreply.github.com>
2024-01-01 19:00:37 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -15,7 +15,7 @@ selected = option_menu(
                             menu_title="Titanic",
                             options=["Home","Dataset","Visualization","Input_form"],
                             icons=["house-fill","database-down","bar-chart", "ui-radios-grid"],
-                            menu_icon="menu-up",
+                            menu_icon="boat",
                             default_index=0,
                             orientation="horizontal"
                       )
