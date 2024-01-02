21e3b34209ff44d4ee7decf863183a97f2937e43
SRIKHANTH R <139539680+Srikhanth14@users.noreply.github.com>
2024-01-03 00:48:54 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,18 +9,13 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 import home,Dataset,Visualization,Input_form
 
-hide_st_style =
-'''
+hide_st_style ="""
 <style>
-
-#MainMenu {visibility: hidden;}
-
-footer {visibility: hidden;}
-header {visibility: hidden;}
-
-</style>
-'''
-
+#MainMenu{visibility:hidden;}
+footer{visibility:hidden;}
+header{visibility:hidden;}
+</style> 
+"""
 st.markdown(hide_st_style,unsafe_allow_html=True)
 
 
